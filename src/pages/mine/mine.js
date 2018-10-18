f64a227c62dfbf768204699390d7bc667ff44376
Lsnsh <zehexin6@163.com>
2018-10-18 23:31:02 +0800

    update: improve style for mine page add initalize some new page

--- src/pages/mine/mine.js
+++ src/pages/mine/mine.js
@@ -1,4 +1,19 @@
 Page({
-  data: {},
+  data: {
+    aMenuList: [
+      {
+        url: '/pages/message/message',
+        open_type: 'navigate',
+        icon_class: 'icon-message',
+        text: '消息'
+      },
+      {
+        url: '/pages/about/about',
+        open_type: 'navigate',
+        icon_class: 'icon-about-us',
+        text: '关于'
+      }
+    ]
+  },
   onLoad: function(options) {}
 });
