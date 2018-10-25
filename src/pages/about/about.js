4990881fb9416d6f27a3bede196aa2978d826978
Lsnsh <zehexin6@163.com>
2018-10-25 19:50:13 +0800

    feat: add project info in about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,4 +1,20 @@
+import { version } from '../../utils/config';
+
 Page({
-  data: {},
-  onLoad: function(options) {}
+  data: {
+    aContentList: [
+      {
+        header: '当前版本',
+        content: `v${version}`
+      },
+      {
+        header: '项目开源主页',
+        content: 'https://github.com/Lsnsh/cnode-wechat-applet'
+      },
+      {
+        header: '关于CNode社区',
+        content: 'https://cnodejs.org/about'
+      }
+    ]
+  }
 });
---
+++ src/utils/config.js
@@ -0,0 +1,1 @@
+export let version = '1.0.0';
