d9c6500e1f0d04b1ed78faf23f66e8e0e45fccb1
Lsnsh <zehexin6@163.com>
2018-11-20 22:29:59 +0800

    update: get message count when initalize application

--- src/app.js
+++ src/app.js
@@ -9,8 +9,31 @@ App({
     this.fnInitMoment();
     this.fnNetVerifyAccessToken();
   },
+  // 获取未读消息数量
+  fnNetGetUnreadMessageCount() {
+    // 显示标题栏加载效果
+    wx.showNavigationBarLoading();
+    wx.dc.message
+      .count()
+      .then(res => {
+        if (res && res.data) {
+          wx.setTabBarBadge({
+            index: 1,
+            text: `${res.data}`
+          });
+        }
+        wx.hideNavigationBarLoading();
+      })
+      .catch(err => {
+        wx.hideNavigationBarLoading();
+      });
+  },
   // 验证access token有效性
   fnNetVerifyAccessToken() {
+    // sAccessToken不存在时，终止执行
+    if (!wx.getStorageSync('sAccessToken')) {
+      return;
+    }
     // 显示标题栏加载效果
     wx.showNavigationBarLoading();
     wx.dc.user
@@ -21,6 +44,8 @@ App({
         }
         // access token有效，更新用户登录状态
         this.globalData.bIsLogin = true;
+        // 获取用户未读消息数量
+        this.fnNetGetUnreadMessageCount();
         wx.hideNavigationBarLoading();
       })
       .catch(err => {
