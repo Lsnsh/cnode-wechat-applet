1ba70bb1a747b378cee4c445cbdbc11575a01882
Lsnsh <zehexin6@163.com>
2018-11-24 23:57:42 +0800

    update: get user profile in user page

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -1,4 +1,28 @@
 Page({
-  data: {},
-  onLoad(options) {}
+  data: {
+    sUserName: ''
+  },
+  onLoad(options) {
+    if (options.name) {
+      this.fnGetUserProfile(options.name);
+      this.setData({
+        sUserName: options.name
+      });
+    }
+  },
+  // 获取用户资料
+  fnGetUserProfile(sUserName) {
+    wx.dc.user
+      .detail({
+        urlData: {
+          loginname: sUserName
+        }
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
